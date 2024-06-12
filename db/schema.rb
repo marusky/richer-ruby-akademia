7e4861c52c5f08a0adbdd378fc43359fcf561abc
marusky <adrianmarusakza@gmail.com>
2024-06-12 10:51:48 +0200

    Add transactions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_10_134247) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_12_082211) do
+  create_table "transactions", force: :cascade do |t|
+    t.string "title"
+    t.integer "kind", default: 0, null: false
+    t.float "amount", default: 0.0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
