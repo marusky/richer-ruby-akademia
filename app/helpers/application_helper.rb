5c8f8ff1a1cc022dbc8b5e4e9f59546af5cbdfb2
Adrián Marušák <adrianmarusakza@gmail.com>
2024-06-12 12:07:12 +0200

    Add transactions (#3)
    * Add transactions
    
    * Add styling
    
    * Add validations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def active_path(path)
+    'active' if request.path == path
+  end
+
+  def active_controller(controller)
+    'active' if controller_name == controller
+  end
+
+  def active_for(path: nil, controller: nil)
+    if path && path == request.path ||
+         controller && controller == controller_name
+      'active'
+    end
+  end
 end
