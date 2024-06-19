eb6102df4225a1bfc999805e165e286f70cbbc0e
marusky <adrianmarusakza@gmail.com>
2024-06-19 13:54:14 +0200

    Add user reference to transactions

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_12_105937) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
