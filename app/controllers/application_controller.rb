d7fc051fe9dbce820ecfbd7713210d081dddbcab
marusky <adrianmarusakza@gmail.com>
2024-06-19 15:03:07 +0200

    Add setup for localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  around_action :wrap_with_locale
+
+  private
+
+  def wrap_with_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+
+    I18n.with_locale(locale, &action)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 
   protected
 
