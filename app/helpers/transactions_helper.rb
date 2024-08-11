6b671d1d7fa3bb53907de0bd3a64be483bee58de
adrian.marusak <adrian.marusak@softpoint.sk>
2024-08-11 11:35:49 +0200

    Add translations

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,12 +1,2 @@
 module TransactionsHelper
-  def number_to_euros(number)
-    number_to_currency(
-      number,
-      unit: '€',
-      separator: ',',
-      delimiter: ' ',
-      format: '%n %u',
-      strip_insignificant_zeros: true
-    )
-  end
 end
